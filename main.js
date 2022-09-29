af99df8b94000f50172619e37d21db8a6f6f893f
Krish <89772346+krishpamecha@users.noreply.github.com>
2022-09-29 15:38:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var speechrecognition=window.webkitSpeechRecognition;
+var recognition=new speechrecognition();
+function start()
+{
+   document.getElementById("textbox").innerHTML="";
+   recognition.start;
+   
+}
